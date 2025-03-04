b132c6c7873eb02f22c9cf03063533b45d5a6550
Roiqk7 <sevecekpeta@gmail.com>
2025-03-04 08:53:19 +0100

    Updatováno pro LA2 (kopie LA1 skriptu)

---
+++ Předměty/linearni-algebra-2/main.py
@@ -0,0 +1,113 @@
+""" 
+Datum: 23/12/2024
+
+Vstupní bod programu
+"""
+
+import argparse
+import random
+import sys
+import time
+
+# Vytvoření parseru pro argumenty v konzoli
+parser = argparse.ArgumentParser()
+
+# Definice argumentů
+parser.add_argument("--vety", help="Zkouška vět", action='store_true')
+parser.add_argument("--temata", help="Zkouška témat", action='store_true')
+parser.add_argument("--vyluc", help="Vyloučí otázky/témata se zadanými indexy [1,2,3,...,N]", type=str)
+parser.add_argument("--cas", help="Časový limit v minutách", type=int)
+
+def main():
+        try:
+                argumenty = parser.parse_args()
+                vylouceneOtazky = []
+                cas = None
+
+                if argumenty.vyluc:
+                        vylouceneOtazky = vylouceneOtazkyParser(argumenty.vyluc)
+                if argumenty.cas:
+                        cas = argumenty.cas
+                if argumenty.vety:
+                        vety(vylouceneOtazky, cas)
+                elif argumenty.temata:
+                        temata(vylouceneOtazky, cas)
+        except Exception as e:
+                vytiskniChybu("Nastala chyba při zpracování argumentů.", e)
+
+def vylouceneOtazkyParser(strVylouceneOtazky):
+        vylouceneOtazkyNezpracovane = [prvek.strip() for prvek in strVylouceneOtazky.strip("[]").split(',')]
+        vylouceneOtazky = []
+        for i in range(len(vylouceneOtazkyNezpracovane)):
+                otazka = vylouceneOtazkyNezpracovane[i]
+                if otazka == "...":
+                        # Případ, kdy je "..." na začátku => vyloučí všechny otázky od 1 do prvního vyloučeného čísla
+                        if i == 0:
+                                for j in range(1, int(vylouceneOtazkyNezpracovane[i+1])):
+                                        vylouceneOtazky.append(j)
+                        # Případ, kdy je "..." na konci => vyloučí všechny otázky od posledního vyloučeného čísla do 30 (maximální počet otázek)
+                        elif i == len(vylouceneOtazkyNezpracovane) - 1:
+                                for j in range(int(vylouceneOtazkyNezpracovane[i-1]), 31):
+                                        vylouceneOtazky.append(j)
+                        else: # Případ, kdy je "..." uprostřed => vyloučí všechny otázky od posledního vyloučeného čísla do dalšího vyloučeného čísla
+                                for j in range(int(vylouceneOtazkyNezpracovane[i-1]), int(vylouceneOtazkyNezpracovane[i+1])):
+                                        vylouceneOtazky.append(j)
+                else: # Číslo otázky
+                        vylouceneOtazky.append(int(otazka))
+        # Odstranění duplikátů (né že by to bylo nutné, ale pro jistotu)
+        vylouceneOtazky = list(set(vylouceneOtazky))
+        return vylouceneOtazky
+
+def vety(vylouceneVety, cas = 10):
+        if cas == None:
+                cas = 10
+        nahodnaVeta = nahodnaOtazka(18, vylouceneVety)
+        veta = prectiOtazkuZeSouboru("vety.txt", nahodnaVeta)
+        print(f"Zformulujte a dokažte: {veta}")
+        casomira(cas)
+
+def temata(vyloucenaTemata, cas = 15):
+        if cas == None:
+                cas = 15
+        nahodneTema = nahodnaOtazka(27, vyloucenaTemata)
+        tema = prectiOtazkuZeSouboru("temata.txt", nahodneTema)
+        print(f"Co víte o tématu: {tema}")
+        print("(definice, vlastnosti, odvození, použití, souvislosti, ...)")
+        casomira(cas)
+
+def nahodnaOtazka(maximalniPocetOtazek, vylouceneOtazky = []):
+        nahodnaOtazka = random.randint(1, maximalniPocetOtazek)
+        # While cyklus zaručuje, že se nevybere otázka, která je vyloučena
+        while nahodnaOtazka in vylouceneOtazky:
+                nahodnaOtazka = random.randint(1, maximalniPocetOtazek)
+        return nahodnaOtazka
+
+def prectiOtazkuZeSouboru(soubor, cisloOtazky):
+        with open(soubor, "r") as soubor:
+                for i, radek in enumerate(soubor, start=1):
+                        if i == cisloOtazky:
+                                return radek.strip()
+
+def vytiskniChybu(zprava, vyjimka = None):
+        print(f"[Chyba] {zprava}")
+        if vyjimka:
+                print(f"Vyjimka: {vyjimka}")
+
+def casomira(minutyCelkem):
+        try:
+                print(f"\nZačíná zkouška. Časový limit: {minutyCelkem} minut.")
+                sekundyCelkem = minutyCelkem * 60
+                for i in range(sekundyCelkem, 0, -15):
+                        minuty, sekundy = divmod(i, 60)
+                        sys.stdout.write(f"\rZbývá: [{minuty:02}:{sekundy:02}]")
+                        sys.stdout.flush()
+                        time.sleep(15)
+                # Odstraní časovač
+                sys.stdout.write("\r" + " " * 20 + "\r")
+                print("\nČas vypršel. Zkouška skončila.")
+        except KeyboardInterrupt:
+                print("\nZkouška byla předběžně ukončena.")
+        print("Zhodnoťte své odpovědi.")
+
+if __name__ == "__main__":
+        main()
---
+++ Předměty/linearni-algebra-2/navod.md
@@ -0,0 +1,46 @@
+# Program na učení se na zkoušku z lineární algebry 1
+
+Program je napsán na učení se na zkoušku z lineární algebry 1. Program obsahuje věty a témata, ze kterých se vybírá náhodně. Je aktuální k zimnímu semestru ročníku 2024/2025 na zkoušku od pana prof. Milana Hladíka.
+
+## Návod na použití
+
+```bash
+usage: main.py [-h] [--vety] [--temata] [--vyluc VYLUC] [--cas CAS]
+```
+
+### Příklady použití
+
+```bash
+python3 main.py --temata
+```
+
+```bash
+python3 main.py --vety --vyluc="[7, ..., 10]"
+```
+
+### Popis argumentů
+
+- `--vety`: začne zkoušku z náhodně vybrané věty
+- `--temata`: začne zkoušku z náhodně vybraného tématu
+- `--vyluc VYLUC`: vyloučí z výběru věty s danými indexy (bráno podle čísla řádku ve zdrojovém souboru)
+- `--cas CAS`: nastaví čas na zkoušku v minutách
+
+#### `--vyluc` argument
+
+Očekává argument ohraničený `""` a `[]`. Prvky v argumentu mohou být čísla nebo `...`. Každý prvek je oddělen čárkou.
+
+- `--vyluc="[10, 13, 15]"`: vyloučí věty z mnnožiny $\{10, 13, 15\}$
+- `--vyluc="[10, ...]"`: vyloučí věty z intervalu $[10, \infty)$
+- `--vyluc="[10, ..., 20, 30]`: vyloučí věty z intervalu $[10, 20] \cup \{30\}$
+- `--vyluc="[... 10, 15]`: vyloučí věty z intervalu $(-\infty, 10] \cup \{15\}$
+
+Tedy `...` lze použít tak, aby se nemuseli vypisovat všechny čísla v intervalu.
+**Pozor**: Indexy jsou číslovány od 1 a čísla ve výčtu jsou vždy včetně.
+
+### Zkouška
+
+Na "zkoušku" je u vět 10 minut a u témat 15 minut. Po uplynutí této doby se program ukončí. Pokud chcete skončit dříve, stiskněte `Ctrl+C`. Program je pro brzké ukončení připraven. Zhodnocení je pak ponecháno na uživateli.
+
+### Snadná úprava
+
+Program je snadné upravit i pro jiné předměty/otázky. Stačí upravit soubor `vety.txt` a `temata.txt` s odpovídajícími větami a tématy.
